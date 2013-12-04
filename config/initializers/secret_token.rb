54f283e5511a9533b0d0bc77070ec03cb4a26b74
Alex Freas <akfreas@gmail.com>
2013-12-04 01:00:51 -0500

    Initial rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sashimisite::Application.config.secret_key_base = 'f813b63251774ed5426b9dd162ae95764fe2e29f4e7a73b38d99838571c93bcf238c20771da3103e867dc3ed2bd58c1a711aa1c58a8dab918810efa941ea4880'
